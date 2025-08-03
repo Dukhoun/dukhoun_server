09e4ef60b9306802068bce25e4c467a1d7bf240a
Dukhoun <ali.s.masadeh123@gmail.com>
2025-08-03 09:12:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     service: 'gmail',
     auth: {
       user: 'dokhuon1@gmail.com',
-      pass: 'zdao jdom xydm ejww'
+      pass: 'urpi ibsn shro vsed'
     }
   },
   stripeSecret: 'sk_test_your_stripe_secret_key'
